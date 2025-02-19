e8e3efe6bfbf6e23dce20e2ddf01b5a5e6815570
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-02-20 02:10:16 +0330

    - Add Manager Class To Models Folder

---
+++ Accounting/Models/Person/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.Abstractions;
+
+namespace Accounting.Models.Person
+{
+    internal class Manager : IPerson
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Gender { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Phone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LandlinePhone { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Email { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Address { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string NationalCode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
