ba8c5e0d49fc5cf3196cad173745a7c28a77a460
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-02-20 02:51:34 +0330

    - Add WindowSound Component To Components Folder

---
+++ Accounting/Ui/Components/WindowSound.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Ui.Components
+{
+    public partial class WindowSound : Component
+    {
+        public WindowSound()
+        {
+            InitializeComponent();
+        }
+
+        public WindowSound(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
