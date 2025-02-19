6fb65244409818023b49525e07624d29b1d3c9af
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-02-20 02:07:41 +0330

    - Update IPerson and Customer Code Base

--- Accounting/Abstractions/IPerson.cs
+++ Accounting/Abstractions/IPerson.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,15 @@ namespace Accounting.Abstractions
 {
     internal interface IPerson
     {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Phone { get; set; }
+        string LandlinePhone { get; set; }
+        string Email { get; set; }
+        string Address { get; set; }
+        string NationalCode { get; set; }
+
 
     }
 }
