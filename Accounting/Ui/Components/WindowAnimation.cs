40d39e8f6828eeca87f2064eb9e0be0f7f4866ae
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-02-20 02:21:52 +0330

    - Add WindowAnimation Component To Componenets Folder

---
+++ Accounting/Ui/Components/WindowAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Ui.Components
+{
+    public partial class WindowAnimation : Component
+    {
+        public WindowAnimation()
+        {
+            InitializeComponent();
+        }
+
+        public WindowAnimation(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
