52f7d1ee7d9854c410735ea3a681e901b7543116
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-02-20 02:29:26 +0330

    - Update WindowAnimation Code Base

--- Accounting/Ui/Components/WindowAnimation.cs
+++ Accounting/Ui/Components/WindowAnimation.cs
@@ -15,6 +15,17 @@ namespace Accounting.Ui.Components
             InitializeComponent();
         }
 
+        public WindowAnimation(System.Windows.Forms.Form window)
+        {
+            this.Window = window;
+            if (this.Window != null)
+            {
+                this.Window.Load += new EventHandler(Start);
+            }
+            timer.Interval = 40;
+            timer.Tick += new EventHandler(this.Timer_Tick);
+        }
+
         public WindowAnimation(IContainer container)
         {
             container.Add(this);
@@ -22,6 +33,31 @@ namespace Accounting.Ui.Components
             InitializeComponent();
         }
 
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
         public System.Windows.Forms.Form Window { get; set; } = null;
+
+        private void Start(object sender, EventArgs e)
+        {
+            this.Window.Opacity = 0;
+            timer.Start();
+        }
+
+        private void Stop()
+        {
+            this.timer.Stop();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (this.Window.Opacity<1)
+            {
+                this.Window.Opacity += 0.10;
+            }
+            else
+            {
+                this.Stop();
+            }
+        }
     }
 }
