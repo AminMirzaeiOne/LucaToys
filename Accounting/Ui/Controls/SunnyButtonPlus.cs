611752cbeeae4f04856452905a462b3bb7c5890d
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-02-22 01:03:56 +0330

    - Update SunnyButtonPlus Code Base

--- Accounting/Ui/Controls/SunnyButtonPlus.cs
+++ Accounting/Ui/Controls/SunnyButtonPlus.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,14 @@ namespace Accounting.Ui.Controls
 {
     public class SunnyButtonPlus : Sunny.UI.UISymbolButton
     {
+        private SoundPlayer player = new SoundPlayer(Properties.Resources.LucaToysLite_Resources_ASound);
         public bool Sound { get; set; } = true;
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            if(this.Sound)
+                player.Play();
+        }
     }
 }
