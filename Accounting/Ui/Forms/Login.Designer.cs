1cb770d08f377caedcdd081cb8a75824c2d0d407
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-02-21 13:21:30 +0330

    - Add Pages Folder To Ui Folder

--- Accounting/Ui/Forms/Login.Designer.cs
+++ Accounting/Ui/Forms/Login.Designer.cs
@@ -31,12 +31,175 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.uiStyleManager1 = new Sunny.UI.UIStyleManager(this.components);
+            this.uiAvatar1 = new Sunny.UI.UIAvatar();
+            this.uiTextBox1 = new Sunny.UI.UITextBox();
+            this.uiTextBox2 = new Sunny.UI.UITextBox();
+            this.uiComboBox1 = new Sunny.UI.UIComboBox();
+            this.uiSymbolButton1 = new Sunny.UI.UISymbolButton();
+            this.uiSymbolButton2 = new Sunny.UI.UISymbolButton();
+            this.uiLine1 = new Sunny.UI.UILine();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uiSymbolButton3 = new Sunny.UI.UISymbolButton();
             this.SuspendLayout();
+            // 
+            // uiAvatar1
+            // 
+            this.uiAvatar1.AvatarSize = 90;
+            this.uiAvatar1.FillColor = System.Drawing.Color.LightGray;
+            this.uiAvatar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.uiAvatar1.Location = new System.Drawing.Point(446, 57);
+            this.uiAvatar1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiAvatar1.Name = "uiAvatar1";
+            this.uiAvatar1.Size = new System.Drawing.Size(120, 106);
+            this.uiAvatar1.SymbolSize = 60;
+            this.uiAvatar1.TabIndex = 0;
+            this.uiAvatar1.Text = "uiAvatar1";
+            // 
+            // uiTextBox1
+            // 
+            this.uiTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiTextBox1.Location = new System.Drawing.Point(286, 224);
+            this.uiTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox1.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTextBox1.Name = "uiTextBox1";
+            this.uiTextBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.uiTextBox1.ShowText = false;
+            this.uiTextBox1.Size = new System.Drawing.Size(263, 59);
+            this.uiTextBox1.TabIndex = 1;
+            this.uiTextBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleRight;
+            this.uiTextBox1.Watermark = "نام کاربری";
+            // 
+            // uiTextBox2
+            // 
+            this.uiTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiTextBox2.Location = new System.Drawing.Point(30, 224);
+            this.uiTextBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox2.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTextBox2.Name = "uiTextBox2";
+            this.uiTextBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.uiTextBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.uiTextBox2.ShowText = false;
+            this.uiTextBox2.Size = new System.Drawing.Size(222, 59);
+            this.uiTextBox2.TabIndex = 1;
+            this.uiTextBox2.TextAlignment = System.Drawing.ContentAlignment.MiddleRight;
+            this.uiTextBox2.Watermark = "رمز عبور";
+            // 
+            // uiComboBox1
+            // 
+            this.uiComboBox1.DataSource = null;
+            this.uiComboBox1.DropDownStyle = Sunny.UI.UIDropDownStyle.DropDownList;
+            this.uiComboBox1.FillColor = System.Drawing.Color.White;
+            this.uiComboBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiComboBox1.ItemHeight = 40;
+            this.uiComboBox1.ItemHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(200)))), ((int)(((byte)(255)))));
+            this.uiComboBox1.Items.AddRange(new object[] {
+            "مدیر",
+            "فروشنده",
+            "مهمان"});
+            this.uiComboBox1.ItemSelectForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.uiComboBox1.Location = new System.Drawing.Point(30, 307);
+            this.uiComboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiComboBox1.MinimumSize = new System.Drawing.Size(63, 0);
+            this.uiComboBox1.Name = "uiComboBox1";
+            this.uiComboBox1.Padding = new System.Windows.Forms.Padding(0, 0, 30, 2);
+            this.uiComboBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.uiComboBox1.Size = new System.Drawing.Size(338, 55);
+            this.uiComboBox1.SymbolSize = 24;
+            this.uiComboBox1.TabIndex = 2;
+            this.uiComboBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiComboBox1.Watermark = "نقش کاربری";
+            // 
+            // uiSymbolButton1
+            // 
+            this.uiSymbolButton1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.uiSymbolButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiSymbolButton1.Location = new System.Drawing.Point(469, 501);
+            this.uiSymbolButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolButton1.Name = "uiSymbolButton1";
+            this.uiSymbolButton1.Radius = 10;
+            this.uiSymbolButton1.Size = new System.Drawing.Size(209, 67);
+            this.uiSymbolButton1.TabIndex = 3;
+            this.uiSymbolButton1.Text = "ورود";
+            this.uiSymbolButton1.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiSymbolButton2
+            // 
+            this.uiSymbolButton2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.uiSymbolButton2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiSymbolButton2.Location = new System.Drawing.Point(19, 501);
+            this.uiSymbolButton2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolButton2.Name = "uiSymbolButton2";
+            this.uiSymbolButton2.Radius = 10;
+            this.uiSymbolButton2.Size = new System.Drawing.Size(209, 67);
+            this.uiSymbolButton2.Symbol = 61534;
+            this.uiSymbolButton2.TabIndex = 3;
+            this.uiSymbolButton2.Text = "انصراف";
+            this.uiSymbolButton2.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // uiLine1
+            // 
+            this.uiLine1.BackColor = System.Drawing.Color.Transparent;
+            this.uiLine1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiLine1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiLine1.Location = new System.Drawing.Point(30, 169);
+            this.uiLine1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiLine1.Name = "uiLine1";
+            this.uiLine1.Size = new System.Drawing.Size(529, 29);
+            this.uiLine1.TabIndex = 4;
+            this.uiLine1.Text = "اطلاعات";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(279, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "امین میرزایی";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 38);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "کد 297";
+            // 
+            // uiSymbolButton3
+            // 
+            this.uiSymbolButton3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.uiSymbolButton3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uiSymbolButton3.Location = new System.Drawing.Point(245, 501);
+            this.uiSymbolButton3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiSymbolButton3.Name = "uiSymbolButton3";
+            this.uiSymbolButton3.Radius = 10;
+            this.uiSymbolButton3.Size = new System.Drawing.Size(209, 67);
+            this.uiSymbolButton3.Symbol = 61546;
+            this.uiSymbolButton3.TabIndex = 3;
+            this.uiSymbolButton3.Text = "مشکل";
+            this.uiSymbolButton3.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             // 
             // Login
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.ClientSize = new System.Drawing.Size(498, 597);
+            this.ClientSize = new System.Drawing.Size(700, 597);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uiLine1);
+            this.Controls.Add(this.uiSymbolButton2);
+            this.Controls.Add(this.uiTextBox1);
+            this.Controls.Add(this.uiTextBox2);
+            this.Controls.Add(this.uiSymbolButton3);
+            this.Controls.Add(this.uiSymbolButton1);
+            this.Controls.Add(this.uiComboBox1);
+            this.Controls.Add(this.uiAvatar1);
             this.EscClose = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IconImageSize = 20;
@@ -51,10 +214,21 @@
             this.TitleHeight = 40;
             this.ZoomScaleRect = new System.Drawing.Rectangle(22, 22, 1175, 600);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private Sunny.UI.UIStyleManager uiStyleManager1;
+        private Sunny.UI.UIAvatar uiAvatar1;
+        private Sunny.UI.UITextBox uiTextBox1;
+        private Sunny.UI.UITextBox uiTextBox2;
+        private Sunny.UI.UIComboBox uiComboBox1;
+        private Sunny.UI.UISymbolButton uiSymbolButton1;
+        private Sunny.UI.UISymbolButton uiSymbolButton2;
+        private Sunny.UI.UILine uiLine1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Sunny.UI.UISymbolButton uiSymbolButton3;
     }
 }
--- Accounting/Ui/Forms/Login.cs
+++ Accounting/Ui/Forms/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sunny.UI;
 
 namespace Accounting.Ui.Forms
 {
@@ -15,7 +16,8 @@ namespace Accounting.Ui.Forms
         public Login()
         {
             InitializeComponent();
-            this.uiStyleManager1.Style = Sunny.UI.UIStyle.Black;
+            this.uiStyleManager1.Style = Sunny.UI.UIStyle.Red;
+            this.uiComboBox1.BackColor = Color.FromArgb(20, 20, 20);
         }
 
 
